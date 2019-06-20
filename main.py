76919e638b430edbc78759d3ffaf16bebbecbcd5
Giusti <andrea.giusti@ewtsrl.com>
2019-06-20 10:26:59 +0200

    moved functions

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import socket
 import sys
 from subprocess import DEVNULL, Popen, PIPE
 from time import sleep
-
+import utils
+from utils import get_status, is_pingable
 from utils import send_talent_mail
 
 try:
@@ -11,8 +12,6 @@ try:
 except FileNotFoundError:
     print("WHOIS command was not found. You need to install whois command on your distro")
     exit(2)
-
-
 
 if len(sys.argv) < 2:
     print("No password found")
@@ -29,32 +28,6 @@ HOSTNAME_DNS = "8.8.8.8"
 HOSTNAME_DNS_PORT = 80
 FILE_ADDRESSES = "ip_addresses"
 
-HEADER = '\033[95m'
-OKBLUE = '\033[94m'
-OKGREEN = '\033[92m'
-WARNING = '\033[93m'
-FAIL = '\033[91m'
-ENDC = '\033[0m'
-BOLD = '\033[1m'
-UNDERLINE = '\033[4m'
-
-
-def is_pingable(ip_address: str):
-    process = Popen(["ping", "-c", "1", ip_address], stdout=PIPE, stderr=DEVNULL)
-    return_code = process.wait()
-    return return_code == 0
-
-
-# function return true if status ok.
-def get_status(domain: str):  # codice nuovo
-    process = Popen(["whois", domain], stdout=PIPE, stderr=DEVNULL)
-    results, _ = process.communicate()
-    results = results.decode("utf-8")  # perchè devo convertire lista di byte in stringa con una certa codifica...
-    marker = results.find("Status: ")
-    line = results[marker:].strip()
-    return "AVAILABLE" in line
-
-
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect((HOSTNAME_DNS, HOSTNAME_DNS_PORT))
 my_ip = s.getsockname()[0]
@@ -65,7 +38,7 @@ lines = file.readlines()
 
 down = 0
 while True:
-    print(HEADER + "Starting scan all ips..." + ENDC)
+    print(utils.HEADER + "Starting scan all ips..." + utils.ENDC)
     sleep(WAITING)  # waiting some seconds
 
     counter = 0
@@ -84,20 +57,20 @@ while True:
         except socket.gaierror:  # If you can't ping, hostname won't exist...
             name_host = host
         if is_pingable(ip):
-            print(OKGREEN + "Success : " + ip + ": " + name_host + ENDC)
+            print(utils.OKGREEN + "Success : " + ip + ": " + name_host + utils.ENDC)
         else:
             if down == 1:  # only first time print
-                print(FAIL + "Fail : " + ip + ": " + name_host + ENDC)
+                print(utils.FAIL + "Fail : " + ip + ": " + name_host + utils.ENDC)
                 res = send_talent_mail(f"{name_host} doesn't respond!", password_gmail, "tommydzepina@gmail.com")
                 if not res:
-                    print(WARNING + "Mail failed" + ENDC)
+                    print(utils.WARNING + "Mail failed" + utils.ENDC)
             elif down == TIMER:  # reset the down counter and reopen the while loop with break
                 down = 0
                 break
 
             if get_status(ip):
                 send_talent_mail(f"{name_host} is AVAILABLE!", password_gmail, "tommydzepina@gmail.com")
-                print(OKGREEN + ip + " Status: is AVAILABLE.")
+                print(utils.OKGREEN + ip + " Status: is AVAILABLE.")
 
     file.close()
 
--- utils.py
+++ utils.py
@@ -1,6 +1,32 @@
 import smtplib
+from subprocess import DEVNULL, Popen, PIPE
 from email.message import EmailMessage
 from datetime import datetime
+
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+
+
+def is_pingable(ip_address: str):
+    process = Popen(["ping", "-c", "1", ip_address], stdout=PIPE, stderr=DEVNULL)
+    return_code = process.wait()
+    return return_code == 0
+
+
+# function return true if status ok.
+def get_status(domain: str):  # codice nuovo
+    process = Popen(["whois", domain], stdout=PIPE, stderr=DEVNULL)
+    results, _ = process.communicate()
+    results = results.decode("utf-8")  # perchè devo convertire lista di byte in stringa con una certa codifica...
+    marker = results.find("Status: ")
+    line = results[marker:].strip()
+    return "AVAILABLE" in line
 
 
 def send_talent_mail(body: str, password: str, destination: str):
@@ -28,19 +54,3 @@ def send_talent_mail(body: str, password: str, destination: str):
     except smtplib.SMTPHeloError:
         print("HELLO message failed.")
     return False
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
